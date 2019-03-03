e38e9df9934c2fa17fdcd9b4f1592f845b726594
Danielle <xdanielle.dsouza@gmail.com>
2019-03-02 20:32:01 -0500

    Removing ng-bootstrap to prevent header error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +18,6 @@ import { FooterComponent } from './footer/footer.component';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    NgbModule,
     AppRoutingModule,
   ],
   providers: [],
