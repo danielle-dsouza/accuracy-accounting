e4a4370f1ccad625299503af4e7bc9737891b715
Danielle <xdanielle.dsouza@gmail.com>
2019-03-11 18:23:37 -0400

    Updates

---
+++ src/app/about-page/about-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Person } from './people.model';
+
+@Component({
+  selector: 'app-about-page',
+  templateUrl: './about-page.component.html',
+  styleUrls: ['./about-page.component.scss']
+})
+export class AboutPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
