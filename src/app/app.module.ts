20b4d6da33ba230972c70e027a9b8dd4618ddf80
Danielle <xdanielle.dsouza@gmail.com>
2019-03-12 21:17:00 -0400

    Adding new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 import { ServicePageComponent } from './service-page/service-page.component';
-import { ServicesComponent } from './service-page/services/services.component';
 import { PeopleComponent } from './about-page/people/people.component';
 import { PeopleListComponent } from './about-page/people/people-list/people-list.component';
+import { ServiceDetailComponent } from './service-page/service-detail/service-detail.component';
+import { ServiceListComponent } from './service-page/service-list/service-list.component';
+import { ServiceItemComponent } from './service-page/service-list/service-item/service-item.component';
 
 @NgModule({
   declarations: [
@@ -25,9 +27,11 @@ import { PeopleListComponent } from './about-page/people/people-list/people-list
     AboutPageComponent,
     ContactPageComponent,
     ServicePageComponent,
-    ServicesComponent,
     PeopleComponent,
-    PeopleListComponent
+    PeopleListComponent,
+    ServiceDetailComponent,
+    ServiceListComponent,
+    ServiceItemComponent
   ],
   imports: [
     BrowserModule,
