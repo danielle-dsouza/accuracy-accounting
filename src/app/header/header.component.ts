62fc3ce23e2b11ee8f70f4c8af66ca58182c5a1d
Danielle <xdanielle.dsouza@gmail.com>
2019-03-15 11:33:58 -0400

    Editing navbar styles

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HostListener, Inject } from "@angular/core";
+import { DOCUMENT } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent implements OnInit {
+  navOpen: boolean = false;
 
-  navOpen:boolean = false;
+  constructor() {}
 
-  constructor() { 
-  }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   toggleNav() {
     this.navOpen = !this.navOpen;
